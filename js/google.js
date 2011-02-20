96116c3fc8d819834ae0cbf2d52c2266133bc4eb
Francesco <favero.francesco@gmail.com>
2011-02-21 00:09:02 +0100

    Playing with Google Map JS API

---
+++ js/google.js
@@ -0,0 +1,87 @@
+var initialLocation;
+var siberia = new google.maps.LatLng(60, 105);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+var geocode;
+var map;
+var infowindow = new google.maps.InfoWindow();
+var rendererOptions = {
+  draggable: true
+};
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
+var directionsService = new google.maps.DirectionsService();
+  
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var myOptions = {
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  
+  // Try W3C Geolocation method (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//      contentString = "Location found using W3C standard";
+      map.setCenter(initialLocation);
+//      infowindow.setContent(contentString);
+//      infowindow.setPosition(initialLocation);
+//      infowindow.open(map);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  } else if (google.gears) {
+    // Try Google Gears Geolocation
+    browserSupportFlag = true;
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+//      contentString = "Location found using Google Gears";
+      map.setCenter(initialLocation);
+//      infowindow.setContent(contentString);
+//      infowindow.setPosition(initialLocation);
+//      infowindow.open(map);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag == true) {
+    initialLocation = newyork;
+    contentString = "Error: The Geolocation service failed.";
+  } else {
+    initialLocation = siberia;
+    contentString = "Error: Your browser doesn't support geolocation. Are you in Siberia?";
+  }
+  map.setCenter(initialLocation);
+//  infowindow.setContent(contentString);
+//  infowindow.setPosition(initialLocation);
+//  infowindow.open(map);
+}
+
+function codeAddress() {
+  var address = document.getElementById("address").value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          draggable:true,
+          animation: google.maps.Animation.DROP,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+
